f5c4703583334c50eab4a0001e3794a726cee62a
Đăng Hưng <danghung24031992@gmail.com>
2015-06-26 12:16:45 +0700

    //update layout

---
+++ app/src/main/java/com/dh/congcusonet/XSBean.java
@@ -0,0 +1,336 @@
+package com.dh.congcusonet;
+
+import java.util.Date;
+
+/**
+ * Created by DH on 6/26/2015.
+ */
+public class XSBean {
+    private int KETQUAID;
+    private Date DATECREATE;
+
+    private String GDB;
+    private String GNHAT;
+    private String GHAI1;
+    private String GHAI2;
+    private String GBA1;
+    private String GBA2;
+    private String GBA3;
+    private String GBA4;
+    private String GBA5;
+    private String GBA6;
+    private String GBON1;
+    private String GBON2;
+    private String GBON3;
+    private String GBON4;
+    private String GNAM1;
+    private String GNAM2;
+    private String GNAM3;
+    private String GNAM4;
+    private String GNAM5;
+    private String GNAM6;
+    private String GSAU1;
+    private String GSAU2;
+    private String GSAU3;
+    private String GBAY1;
+    private String GBAY2;
+    private String GBAY3;
+    private String GBAY4;
+    private String KQLOCUOI;
+    private String KQLODAU;
+    private String CHUOISO;
+
+    public XSBean() {
+    }
+
+    public XSBean(int KETQUAID, Date DATECREATE, String GDB, String GNHAT, String GHAI1, String GHAI2, String GBA1, String GBA2, String GBA3, String GBA4, String GBA5, String GBA6, String GBON1, String GBON2, String GBON3, String GBON4, String GNAM1, String GNAM2, String GNAM3, String GNAM4, String GNAM5, String GNAM6, String GSAU1, String GSAU2, String GSAU3, String GBAY1, String GBAY2, String GBAY3, String GBAY4, String KQLOCUOI, String KQLODAU, String CHUOISO) {
+        this.KETQUAID = KETQUAID;
+        this.DATECREATE = DATECREATE;
+        this.GDB = GDB;
+        this.GNHAT = GNHAT;
+        this.GHAI1 = GHAI1;
+        this.GHAI2 = GHAI2;
+        this.GBA1 = GBA1;
+        this.GBA2 = GBA2;
+        this.GBA3 = GBA3;
+        this.GBA4 = GBA4;
+        this.GBA5 = GBA5;
+        this.GBA6 = GBA6;
+        this.GBON1 = GBON1;
+        this.GBON2 = GBON2;
+        this.GBON3 = GBON3;
+        this.GBON4 = GBON4;
+        this.GNAM1 = GNAM1;
+        this.GNAM2 = GNAM2;
+        this.GNAM3 = GNAM3;
+        this.GNAM4 = GNAM4;
+        this.GNAM5 = GNAM5;
+        this.GNAM6 = GNAM6;
+        this.GSAU1 = GSAU1;
+        this.GSAU2 = GSAU2;
+        this.GSAU3 = GSAU3;
+        this.GBAY1 = GBAY1;
+        this.GBAY2 = GBAY2;
+        this.GBAY3 = GBAY3;
+        this.GBAY4 = GBAY4;
+        this.KQLOCUOI = KQLOCUOI;
+        this.KQLODAU = KQLODAU;
+        this.CHUOISO = CHUOISO;
+    }
+
+    public int getKETQUAID() {
+        return KETQUAID;
+    }
+
+    public void setKETQUAID(int KETQUAID) {
+        this.KETQUAID = KETQUAID;
+    }
+
+    public Date getDATECREATE() {
+        return DATECREATE;
+    }
+
+    public void setDATECREATE(Date DATECREATE) {
+        this.DATECREATE = DATECREATE;
+    }
+
+    public String getGDB() {
+        return GDB;
+    }
+
+    public void setGDB(String GDB) {
+        this.GDB = GDB;
+    }
+
+    public String getGNHAT() {
+        return GNHAT;
+    }
+
+    public void setGNHAT(String GNHAT) {
+        this.GNHAT = GNHAT;
+    }
+
+    public String getGHAI1() {
+        return GHAI1;
+    }
+
+    public void setGHAI1(String GHAI1) {
+        this.GHAI1 = GHAI1;
+    }
+
+    public String getGHAI2() {
+        return GHAI2;
+    }
+
+    public void setGHAI2(String GHAI2) {
+        this.GHAI2 = GHAI2;
+    }
+
+    public String getGBA1() {
+        return GBA1;
+    }
+
+    public void setGBA1(String GBA1) {
+        this.GBA1 = GBA1;
+    }
+
+    public String getGBA2() {
+        return GBA2;
+    }
+
+    public void setGBA2(String GBA2) {
+        this.GBA2 = GBA2;
+    }
+
+    public String getGBA3() {
+        return GBA3;
+    }
+
+    public void setGBA3(String GBA3) {
+        this.GBA3 = GBA3;
+    }
+
+    public String getGBA4() {
+        return GBA4;
+    }
+
+    public void setGBA4(String GBA4) {
+        this.GBA4 = GBA4;
+    }
+
+    public String getGBA5() {
+        return GBA5;
+    }
+
+    public void setGBA5(String GBA5) {
+        this.GBA5 = GBA5;
+    }
+
+    public String getGBA6() {
+        return GBA6;
+    }
+
+    public void setGBA6(String GBA6) {
+        this.GBA6 = GBA6;
+    }
+
+    public String getGBON1() {
+        return GBON1;
+    }
+
+    public void setGBON1(String GBON1) {
+        this.GBON1 = GBON1;
+    }
+
+    public String getGBON2() {
+        return GBON2;
+    }
+
+    public void setGBON2(String GBON2) {
+        this.GBON2 = GBON2;
+    }
+
+    public String getGBON3() {
+        return GBON3;
+    }
+
+    public void setGBON3(String GBON3) {
+        this.GBON3 = GBON3;
+    }
+
+    public String getGBON4() {
+        return GBON4;
+    }
+
+    public void setGBON4(String GBON4) {
+        this.GBON4 = GBON4;
+    }
+
+    public String getGNAM1() {
+        return GNAM1;
+    }
+
+    public void setGNAM1(String GNAM1) {
+        this.GNAM1 = GNAM1;
+    }
+
+    public String getGNAM2() {
+        return GNAM2;
+    }
+
+    public void setGNAM2(String GNAM2) {
+        this.GNAM2 = GNAM2;
+    }
+
+    public String getGNAM3() {
+        return GNAM3;
+    }
+
+    public void setGNAM3(String GNAM3) {
+        this.GNAM3 = GNAM3;
+    }
+
+    public String getGNAM4() {
+        return GNAM4;
+    }
+
+    public void setGNAM4(String GNAM4) {
+        this.GNAM4 = GNAM4;
+    }
+
+    public String getGNAM5() {
+        return GNAM5;
+    }
+
+    public void setGNAM5(String GNAM5) {
+        this.GNAM5 = GNAM5;
+    }
+
+    public String getGNAM6() {
+        return GNAM6;
+    }
+
+    public void setGNAM6(String GNAM6) {
+        this.GNAM6 = GNAM6;
+    }
+
+    public String getGSAU1() {
+        return GSAU1;
+    }
+
+    public void setGSAU1(String GSAU1) {
+        this.GSAU1 = GSAU1;
+    }
+
+    public String getGSAU2() {
+        return GSAU2;
+    }
+
+    public void setGSAU2(String GSAU2) {
+        this.GSAU2 = GSAU2;
+    }
+
+    public String getGSAU3() {
+        return GSAU3;
+    }
+
+    public void setGSAU3(String GSAU3) {
+        this.GSAU3 = GSAU3;
+    }
+
+    public String getGBAY1() {
+        return GBAY1;
+    }
+
+    public void setGBAY1(String GBAY1) {
+        this.GBAY1 = GBAY1;
+    }
+
+    public String getGBAY2() {
+        return GBAY2;
+    }
+
+    public void setGBAY2(String GBAY2) {
+        this.GBAY2 = GBAY2;
+    }
+
+    public String getGBAY3() {
+        return GBAY3;
+    }
+
+    public void setGBAY3(String GBAY3) {
+        this.GBAY3 = GBAY3;
+    }
+
+    public String getGBAY4() {
+        return GBAY4;
+    }
+
+    public void setGBAY4(String GBAY4) {
+        this.GBAY4 = GBAY4;
+    }
+
+    public String getKQLOCUOI() {
+        return KQLOCUOI;
+    }
+
+    public void setKQLOCUOI(String KQLOCUOI) {
+        this.KQLOCUOI = KQLOCUOI;
+    }
+
+    public String getKQLODAU() {
+        return KQLODAU;
+    }
+
+    public void setKQLODAU(String KQLODAU) {
+        this.KQLODAU = KQLODAU;
+    }
+
+    public String getCHUOISO() {
+        return CHUOISO;
+    }
+
+    public void setCHUOISO(String CHUOISO) {
+        this.CHUOISO = CHUOISO;
+    }
+}
